bd04e92ff54d1267f64ff9c21ac813896f5b3e67
Chiedza Ikpeh <151172756+chiedza-ikpeh@users.noreply.github.com>
2023-11-27 15:17:04 +0000

    Update milestone_5_refactored.py

--- milestone_5_refactored.py
+++ milestone_5_refactored.py
@@ -1,19 +1,52 @@
-def play_game(word_list):
-    num_lives = 5
-    game = Hangman(word_list, num_lives)
+def start_hangman_game(word_list):
+    """
+    Initiates and controls the flow of the Hangman game.
+    :param word_list: List of words to be used in the game.
+    """
+    initial_lives = 5
+    hangman_game = Hangman(word_list, initial_lives)
 
     while True:
-        if game.num_lives == 0:
-            print('You lost!')
-            print(f"The word was: {game.word}")
+        if _is_game_lost(hangman_game):
+            _announce_game_loss(hangman_game.word)
             break
 
-        if game.num_letters > 0:
-            game.ask_for_input()
+        if _continue_game(hangman_game):
+            hangman_game.ask_for_input()
         else:
-            print('Congratulations. You won the game!')
+            _announce_game_win()
             break
+
+def _is_game_lost(game):
+    """
+    Checks if the game is lost.
+    :param game: The Hangman game instance.
+    :return: True if the game is lost, otherwise False.
+    """
+    return game.num_lives == 0
+
+def _announce_game_loss(correct_word):
+    """
+    Announces that the player has lost the game.
+    :param correct_word: The correct word that was to be guessed.
+    """
+    print('You lost!')
+    print(f"The word was: {correct_word}")
+
+def _continue_game(game):
+    """
+    Determines if the game should continue.
+    :param game: The Hangman game instance.
+    :return: True if the game should continue, otherwise False.
+    """
+    return game.num_letters > 0
+
+def _announce_game_win():
+    """
+    Announces that the player has won the game.
+    """
+    print('Congratulations. You won the game!')
 
 # Example usage
 word_list = ["apple", "banana", "cherry"]
-play_game(word_list)
+start_hangman_game(word_list)
