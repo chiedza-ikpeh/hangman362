bb5923afaf9055fe748a6a2dc70e93047ba63481
Chiedza Ikpeh <151172756+chiedza-ikpeh@users.noreply.github.com>
2023-11-27 14:58:30 +0000

    Update milestone_4.py

--- milestone_4.py
+++ milestone_4.py
@@ -1,7 +1,8 @@
 # Milestone_4
-# Task
+
 import random
 
+# Task 1
 class Hangman:
     def __init__(self, word_list, num_lives=5):
         # Initialize attributes
@@ -22,3 +23,67 @@ print("Word Guessed:", game.word_guessed)
 print("Number of Unique Letters:", game.num_letters)
 print("Number of Lives:", game.num_lives)
 print("List of Guesses:", game.list_of_guesses)
+
+# Task 2
+class Hangman:
+    # ... (previous code for __init__, ask_for_input)
+    def check_guess(self, guess):
+        guess = guess.lower()
+        if guess in self.word:
+            print(f"Good guess! {guess} is in the word.")
+            
+            # Loop through each letter in the word
+            for i, letter in enumerate(self.word):
+                # Check if the letter is equal to the guess
+                if letter == guess:
+                    # Replace the corresponding "_" in word_guessed with the guess
+                    self.word_guessed[i] = guess
+
+            # Reduce num_letters by 1 only if this is a new letter
+            if self.word_guessed.count(guess) == 1:
+                self.num_letters -= 1
+        # Additional logic for incorrect guess will be implemented in future tasks
+
+# Task 3
+class Hangman:
+    # ... (previous code for __init__, ask_for_input)
+
+    def check_guess(self, guess):
+        guess = guess.lower()
+        if guess in self.word:
+            print(f"Good guess! {guess} is in the word.")
+            
+            # Variable to track if the letter was newly guessed
+            letter_newly_guessed = False
+            
+            # Loop through each letter in the word
+            for i, letter in enumerate(self.word):
+                # Check if the letter is equal to the guess
+                if letter == guess:
+                    # Replace the corresponding "_" in word_guessed with the guess
+                    if self.word_guessed[i] == '_':
+                        letter_newly_guessed = True
+                    self.word_guessed[i] = guess
+
+            # Reduce num_letters by 1 if the letter was newly guessed
+            if letter_newly_guessed:
+                self.num_letters -= 1
+        # Additional logic for incorrect guess will be implemented in future tasks
+
+# Task 4
+class Hangman:
+    # ... (previous code for __init__, ask_for_input)
+
+    def check_guess(self, guess):
+        guess = guess.lower()
+        if guess in self.word:
+            # (existing code for handling correct guess)
+        else:
+            # Step 1: Else statement for handling incorrect guess
+            self.num_lives -= 1  # Step 2: Reduce num_lives by 1
+
+            # Step 3: Print messages
+            print(f"Sorry, {guess} is not in the word.")
+            print(f"You have {self.num_lives} lives left.")
+            # Additional handling for when num_lives reaches 0 can be added later
+
