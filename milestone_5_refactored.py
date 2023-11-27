323d4fe306f70e58578453347b44f007218c838a
Chiedza Ikpeh <151172756+chiedza-ikpeh@users.noreply.github.com>
2023-11-27 15:14:49 +0000

    Create milestone_5_refactored.py

---
+++ milestone_5_refactored.py
@@ -0,0 +1,19 @@
+def play_game(word_list):
+    num_lives = 5
+    game = Hangman(word_list, num_lives)
+
+    while True:
+        if game.num_lives == 0:
+            print('You lost!')
+            print(f"The word was: {game.word}")
+            break
+
+        if game.num_letters > 0:
+            game.ask_for_input()
+        else:
+            print('Congratulations. You won the game!')
+            break
+
+# Example usage
+word_list = ["apple", "banana", "cherry"]
+play_game(word_list)
