7e46e274d335452a49519a2521f3966d2c2588a4
Chiedza Ikpeh <151172756+chiedza-ikpeh@users.noreply.github.com>
2023-11-27 11:25:32 +0000

    milestone_2.py

---
+++ milestone_2.py
@@ -0,0 +1,5 @@
+# Step 1 & 2: Create a list of 5 favorite fruits and assign it to a variable called word_list
+word_list = ["Apple", "Banana", "Cherry", "Grapes", "Mango"]
+
+# Step 3: Print the list
+print(word_list)
